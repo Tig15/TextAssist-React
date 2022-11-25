0d7485b51a4f5dfc546c965a1e09997ee0ba7ec6
Tig15 <amanvgec131@gmail.com>
2022-11-25 22:14:36 +0530

    Updates

---
+++ precache-manifest.8a31357e0168ab9da9a1534f3a66f856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec5fa3df5fd8b2fccd22",
+    "url": "/TextAssist-React/static/css/main.957606a4.chunk.css"
+  },
+  {
+    "revision": "ec5fa3df5fd8b2fccd22",
+    "url": "/TextAssist-React/static/js/main.ec5fa3df.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextAssist-React/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextAssist-React/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "f44e96a585710fb58b3b",
+    "url": "/TextAssist-React/static/js/runtime~main.f44e96a5.js"
+  },
+  {
+    "revision": "cacdfb5b03ca434da355c9fb3ae44558",
+    "url": "/TextAssist-React/index.html"
+  }
+];
